b8dd6ad95306b574227c8c34de552304c250955a
Nirex <akay93796@gmail.com>
2024-02-07 19:53:36 +0530

    fixes

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Valentine",
+  title: "Valentine - for my maal😁",
   description: "valentine",
 };
 
